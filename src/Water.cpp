578b29d5f302ad5e68d3bd1029bde83b6374100a
Alwin Esch <alwin.esch@web.de>
2017-05-22 03:04:20 +0200

    Update on kodi to addon interface

--- src/Water.cpp
+++ src/Water.cpp
@@ -238,7 +238,7 @@ ADDON_STATUS ADDON_Create(void* hdl, void* props)
     return ADDON_STATUS_PERMANENT_FAILURE;
   }
 
-  SCR_PROPS* scrprops = (SCR_PROPS*)props;
+  AddonProps_Screensaver* scrprops = (AddonProps_Screensaver*)props;
 
   m_iWidth = scrprops->width;
   m_iHeight  = scrprops->height;
@@ -344,7 +344,7 @@ extern "C" void Render()
 // XBMC tells us to stop the screensaver
 // we should free any memory and release
 // any resources we have created.
-extern "C" void ADDON_Stop()
+extern "C" void Stop()
 {
   if (gTexture != 0)
     glDeleteTextures(1, &gTexture);
