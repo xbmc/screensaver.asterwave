2c1387cf34184e0983a1fd78513d0a00eb104428
Alwin Esch <alwin.esch@web.de>
2020-05-15 19:10:06 +0200

    change source license to SPDX format

--- src/Util.h
+++ src/Util.h
@@ -1,3 +1,11 @@
+/*
+ *  Copyright (C) 2005-2020 Team Kodi (https://kodi.tv)
+ *  Copyright (C) 2007 Asteron (http://asteron.projects.googlepages.com/home)
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later
+ *  See LICENSE.md for more information.
+ */
+
 #pragma once
 
 #include "types.h"
